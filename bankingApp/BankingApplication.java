b893acac36eaaa31544041f8675aa6c2d2724f7e
Mutabazi <dgibrillymutabazi@gmail.com>
2023-12-26 19:44:22 +0200

    Welcome screen

--- bankingApp/BankingApplication.java
+++ bankingApp/BankingApplication.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class BankingApplication{
 
 
@@ -8,6 +10,12 @@ class BankAccount{
     int previousTransaction;
     String customerName;
     String customerId ;
+
+    // constructor to initialize user Account
+    BankAccount(String cname, String cId){
+        customerName = cname ; 
+        customerId = cId ;
+    }
 
     // deposit money
     void deposit(int amount){
@@ -24,11 +32,49 @@ class BankAccount{
         }
     }
     // return statetement of previous transaction
-
     void getPreviousTransaction(){
         if(previousTransaction > 0 ){
-            System.out.println();
+            System.out.println("Deposited: "+previousTransaction);
+        }else if(previousTransaction < 0 ){
+            System.out.println("Withdrawn: "+Math.abs(previousTransaction)) ;
+        }else{
+            System.out.println("No transaction occurred");
         }
     }
+
+    //display menu to user
+    void showMenu(){
+        char option = '\0' ;  //initialize character 
+        Scanner scanner = new Scanner(System.in) ;  //read user input
+
+        //welcome message
+        System.out.println("Welcome "+customerName);
+        System.out.println("Your ID is "+customerId) ;
+        System.out.println("\n") ; 
+
+        System.out.println("A. Check Balance") ;
+        System.out.println("B. Deposit");
+        System.out.println("C. Withdraw");
+        System.out.println("D. Previous Transaction");
+        System.out.println("E. Exit");
+
+        // do loop used, to read the input from a user once atleast
+        do{
+            System.out.println("=============================================================================");
+            System.out.println("Enter an option: ") ; 
+            System.out.println("=============================================================================");
+            option = scanner.next().charAt(0) ; // recieve user input
+            System.out.println("\n") ;
+
+            switch(option){
+
+            }
+        }while( option != 'E');
+        
+
+
+    }
+
+
     
 }
