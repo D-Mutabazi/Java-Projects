eb0160b1e0e8fdaf80aa9cf8ba763f453095a614
Mutabazi <dgibrillymutabazi@gmail.com>
2023-12-27 00:55:31 +0200

    Operations functional on for this banking program

--- bankingApp/BankingApplication.java
+++ bankingApp/BankingApplication.java
@@ -1,90 +1,120 @@
-import java.util.Scanner;
-
 public class BankingApplication{
-
+    public static void main(String[] args){
+        BankAccount obj1 = new BankAccount("XYZ", "BA0001") ;
+        obj1.showMenu();
+    }
 
 }
 
+/* 
 class BankAccount{
-    int balance ; 
-    int previousTransaction;
-    String customerName;
-    String customerId ;
 
-    // constructor to initialize user Account
-    BankAccount(String cname, String cId){
-        customerName = cname ; 
-        customerId = cId ;
-    }
+    // int balance ; // store account balance
+    // int previousTransaction;
+    // String customerName;
+    // String customerId ;
 
-    // deposit money
-    void deposit(int amount){
-        if(amount != 0){
-            balance = balance +amount ; 
-            previousTransaction = amount;
-        }
-    }
-    // withdraw money
-    void withdraw(int amount){
-        if(amount != 0 ){
-            balance = balance - amount ;
-            previousTransaction = -amount ; //keep track of amount withdrawn per transaction
-        }
-    }
-    // return statetement of previous transaction
-    void getPreviousTransaction(){
-        if(previousTransaction > 0 ){
-            System.out.println("Deposited: "+previousTransaction);
-        }else if(previousTransaction < 0 ){
-            System.out.println("Withdrawn: "+Math.abs(previousTransaction)) ;
-        }else{
-            System.out.println("No transaction occurred");
-        }
-    }
+    // // constructor to initialize user Account
+    // BankAccount(String cname, String cId){
+    //     customerName = cname ; 
+    //     customerId = cId ;
+    // }
 
-    //display menu to user
-    void showMenu(){
-        char option = '\0' ;  //initialize character 
-        Scanner scanner = new Scanner(System.in) ;  //read user input
+    // // deposit money
+    // void deposit(int amount){
+    //     if(amount != 0){
+    //         balance = balance +amount ; 
+    //         previousTransaction = amount;
+    //     }
+    // }
+    // // withdraw money
+    // void withdraw(int amount){
+    //     if(amount != 0 ){
+    //         balance = balance - amount ;
+    //         previousTransaction = -amount ; //keep track of amount withdrawn per transaction
+    //     }
+    // }
+    // // return statetement of previous transaction
+    // void getPreviousTransaction(){
+    //     if(previousTransaction > 0 ){
+    //         System.out.println("Deposited: "+previousTransaction);
+    //     }else if(previousTransaction < 0 ){
+    //         System.out.println("Withdrawn: "+Math.abs(previousTransaction)) ;
+    //     }else{
+    //         System.out.println("No transaction occurred");
+    //     }
+    // }
 
-        //welcome message
-        System.out.println("Welcome "+customerName);
-        System.out.println("Your ID is "+customerId) ;
-        System.out.println("\n") ; 
+    // //display menu to user
+    // void showMenu(){
+    //     char option = '\0' ;  //initialize character 
+    //     Scanner scanner = new Scanner(System.in) ;  //read user input
 
-        System.out.println("A. Check Balance") ;
-        System.out.println("B. Deposit");
-        System.out.println("C. Withdraw");
-        System.out.println("D. Previous Transaction");
-        System.out.println("E. Exit");
+    //     //welcome message
+    //     System.out.println("Welcome "+customerName);
+    //     System.out.println("Your ID is "+customerId) ;
+    //     System.out.println("\n") ; 
 
-        // do loop used, to read the input from a user once atleast
-        do{
-            System.out.println("=============================================================================");
-            System.out.println("Enter an option: ") ; 
-            System.out.println("=============================================================================");
-            option = scanner.next().charAt(0) ; // recieve user input
-            System.out.println("\n") ;
+    //     System.out.println("A. Check Balance") ;
+    //     System.out.println("B. Deposit");
+    //     System.out.println("C. Withdraw");
+    //     System.out.println("D. Previous Transaction");
+    //     System.out.println("E. Exit");
 
-            switch(option){
-                case 'A':
-                    System.out.println("-------------------------------------------");
-                    System.out.println("Balance: "+balance);
-                    System.out.println("-------------------------------------------");
-                    System.out.println("\n");
-                    break ;
-                    case 'B':
-                    Syse
+    //     // do loop used, to read the input from a user once atleast
+    //     do{
+    //         System.out.println("=============================================================================");
+    //         System.out.println("Enter an option: ") ; 
+    //         System.out.println("=============================================================================");
+    //         option = scanner.next().charAt(0) ; // recieve user input
+    //         System.out.println("\n") ;
 
-            }
-        }while( option != 'E');
+    //         switch(option){
+    //             case 'A':
+    //                 System.out.println("-------------------------------------------");
+    //                 System.out.println("Balance: "+balance);
+    //                 System.out.println("-------------------------------------------");
+    //                 System.out.println("\n");
+    //                 break ;
+    //             case 'B':
+    //                 System.out.println("-------------------------------------------");
+    //                 System.out.println("Enter an amount to deposit: ");
+    //                 System.out.println("-------------------------------------------");
+    //                 int amount  = scanner.nextInt() ; 
+    //                 deposit(amount);
+    //                 System.out.println("\n") ; 
+    //                 break;
+    //             case 'C':
+    //                 System.out.println("-------------------------------------------");
+    //                 System.out.println("Enter an amount to withdraw") ; 
+    //                 System.out.println("-------------------------------------------");
+    //                 int amount2 = scanner.nextInt() ;
+    //                 withdraw(amount2);
+    //                 System.out.println("\n");
+    //                 break;
+                
+    //             case 'D':
+    //                 System.out.println("-------------------------------------------");
+    //                 getPreviousTransaction();
+    //                 System.out.println("-------------------------------------------");
+    //                 System.out.println("\n");
+    //                 break;
 
-        System.out.println(customerId);
+    //             case 'E':
+    //                 System.out.println("******************************************************");
+    //                 break;
+
+    //             default:
+    //                 System.out.println("Invalid Option!!. Please enter again") ;
+    //                 break;     
+
+    //         }
+    //     }while( option != 'E');
+
+    //     System.out.println(customerId);
 
 
 
-    }
-
-
-    
+    // }
 }
+*/
---
+++ bankingApp/README.md
@@ -0,0 +1,6 @@
+This is a simple banking application, where the operations that can be performed include;
+* Checking Balance
+* Depositing 
+* Withdrawing
+* Viewing previous occurred transaction
+and finally, the user has the option of exiting the program.
